c369ddaf14f3d4b0a69dcab0dba88ca38492ef21
Tyago Teoi <tyago.teoi@usp.br>
2022-02-26 12:01:41 -0300

    Add knockback for knight using statemachine

--- Assets/Scripts/Knight/BehaviorStatemachine/Knight_Knockback.cs
+++ Assets/Scripts/Knight/BehaviorStatemachine/Knight_Knockback.cs
@@ -3,11 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Knight_Knockback : StateMachineBehaviour
-{    
+{
+    public float knockbackForceX;
+    public float knockbackForceY;
+
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        Rigidbody2D rb = animator.GetComponent<Rigidbody2D>();
+        Transform player = GameObject.FindGameObjectWithTag("Player").transform;
+
         animator.GetComponent<LifeEnemy>().recovering = true;
+
+        if (rb.transform.position.x < player.position.x)
+        {
+            rb.AddForce(new Vector2(-knockbackForceX, knockbackForceY), ForceMode2D.Impulse);
+        }
+        else
+        {
+            rb.AddForce(new Vector2(knockbackForceX, knockbackForceY), ForceMode2D.Impulse);
+        }
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
