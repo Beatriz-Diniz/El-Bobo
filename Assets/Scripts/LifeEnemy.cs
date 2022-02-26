f037a2a45a38dd1948d0f82a0684cb82c8e1f767
Tyago Teoi <tyago.teoi@usp.br>
2022-02-26 11:15:00 -0300

    Add knockback state behavior for knight

--- Assets/Scripts/LifeEnemy.cs
+++ Assets/Scripts/LifeEnemy.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class LifeEnemy : MonoBehaviour
 {
-    private bool recovering;
-    public int health;
+    public bool recovering = false;                 // estah invulneravel nesse tempo
 
-    public float recoveryTime;
+    //public float recoveryTime;                    // OBS: pensei em nao usar essa variavel e
+                                                    // tomar automaticamente como recoveryTime
+                                                    // o tempo da animacao do knockback
+
+    public int health;
 
     //para criar a barra de hp
     public Transform healthBar;                     //barra verde
@@ -34,7 +37,8 @@ public class LifeEnemy : MonoBehaviour
     void Update()
     {
         //animacao de morte e destruir inimigo
-        if (health <= 0) {
+        if (health <= 0) 
+        {
             animator.SetTrigger("Morrendo");
             Debug.Log("Soldado morreu por vida");
             Destroy(inimigo, tempoAnimacaoMorte);
@@ -43,22 +47,28 @@ public class LifeEnemy : MonoBehaviour
     }
 
 
-    void UpdateHealthBar(){
+    void UpdateHealthBar() 
+    {
         healthBarScale.x = healthPercent * health;
         healthBar.localScale = healthBarScale;
     }
 
-    public void TakeDamage(int damage){
-        if(!recovering){
+    public void TakeDamage(int damage) {
+        if(!recovering) 
+        {
             //animacao do inimigo sofrendo dano
+            GetComponent<Animator>().SetTrigger("Knockback");
+
             Debug.Log("Tomou dano");
             health -= damage;
             UpdateHealthBar();
         }
     }
     
-    void OnCollisionEnter2D(Collision2D other){
-        if(other.gameObject.CompareTag("Player")){
+    void OnCollisionEnter2D(Collision2D other) 
+    {
+        if(other.gameObject.CompareTag("Player")) 
+        {
             other.gameObject.GetComponent<SimpleMove>().TakeDamage(dano);
         }
     }
